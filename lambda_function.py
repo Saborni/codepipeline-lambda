ee35c523cb3087498d742a9749098bf5518934f1
Saborni <bhattacharyasab93@gmail.com>
2025-10-02 16:53:35 +0100

    new test

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+def lambda_handler(event, context):
+    response = requests.get('https://example.com/')
+    message = 'site is Down!'
+    if response.status_code == 200:
+        message = 'site is Up!'
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': message,
+            'version': '1.0'
+        })
+    }
